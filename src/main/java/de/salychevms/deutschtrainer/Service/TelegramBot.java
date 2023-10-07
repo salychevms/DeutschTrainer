48b41677a6fc6ccb4f77fb0cb3833819836c8de1
Mikhail Salychev <salychevms@gmail.com>
2023-10-07 15:38:07 +0200

    different registration

--- src/main/java/de/salychevms/deutschtrainer/Service/TelegramBot.java
+++ src/main/java/de/salychevms/deutschtrainer/Service/TelegramBot.java
@@ -5,10 +5,13 @@ import de.salychevms.deutschtrainer.Controllers.UsersController;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,12 +22,13 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     final BotConfig config;
     final UsersController usersController;
+    private long chatId;
+    private String userName;
 
     public TelegramBot(BotConfig config, UsersController usersController) {
         this.config = config;
         this.usersController = usersController;
     }
-
 
     @Override
     public String getBotUsername() {
@@ -38,61 +42,80 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        if (update.hasMessage() && update.getMessage().hasText()) {
-            String messageText = update.getMessage().getText();
-            //get userId
-            long chatId = update.getMessage().getChatId();
-            //get userName, if it hides we get default userName
-            String userName = update.getMessage().getChat().getUserName() != null ? update.getMessage().getChat().getUserName() : "NONAME";
+        //check message
+        if (update.hasCallbackQuery()) {
+            CallbackQuery callbackQuery = update.getCallbackQuery();
+            String callBackData = callbackQuery.getData();
 
-            switch (messageText) {
-                case "/start":
+            if (callBackData.equals("/registration")) {
+                if (!usersController.registeredOr(userName)) {
+                    usersController.createNewUser(userName);
+                    if (usersController.registeredOr(userName)) {
+                        sendMessage(chatId, "You're already registered!");
+                    } else
+                        sendMessage(chatId, "Sorry! Something bad happened! Write \"/start\" and try again!");
+                } else sendMessage(chatId, "You're already registered!");
+            } else if (callBackData.equals("/aboutRegistration")) {
+                if (!usersController.registeredOr(userName)) {
+                    sendMessage(chatId, "You have to registered before using this bot!\nYou have to just click \"Registration\"!");
+                } else sendMessage(chatId, "You're already registered!");
+            }
+        } else if (update.hasMessage()) {
+            Message message = update.getMessage();
+            if (message.hasText()) {
+                String messageText = message.getText();
+                chatId = message.getChatId();
+                userName = message.getChat().getUserName() != null ? message.getChat().getUserName() : "NONAME";
+                if (messageText.equals("/start")) {
                     sendMessage(chatId, "Hi, " + userName + "! You activated this bot.");
                     if (!usersController.registeredOr(userName)) {
-                        String msg="You're not registered before!\nPlease, register to use";
-                        ReplyKeyboardMarkup keyboardMarkup=new ReplyKeyboardMarkup();
-                        List<KeyboardRow> rows=new ArrayList<>();
-                        KeyboardRow row=new KeyboardRow();
-                        row.add("Registration");
-                        rows.add(row);
-                        row=new KeyboardRow();
-                        row.add("About registration");
-                        rows.add(row);
-                        keyboardMarkup.setKeyboard(rows);
-                        sendRegButton(chatId,msg, keyboardMarkup);
+                        try {
+                            execute(registration(chatId));
+                        } catch (TelegramApiException e) {
+                            throw new RuntimeException(e);
+                        }
                     }
-                    break;
-                case "Registration":
-                    usersController.createNewUser(userName);
-                    if(usersController.registeredOr(userName)){
-                        sendMessage(chatId,"You're already registered!");
-                    }else sendMessage(chatId, "Sorry! Something bad happened! Write \"/start\" and try again!");
-                    break;
-                case "About registration":
-                    sendMessage(chatId,"You have to registered before using this bot!\nYou have to just click \"Registration\"!");
-                    break;
-                default:
+                } else {
                     sendMessage(chatId, "Sorry! It does not works.");
+                }
             }
         }
+    }
+
+    private SendMessage registration(long chatId) {
+        String msg = "You're not registered before!\nPlease, register to use";
+        InlineKeyboardMarkup keyboardMarkup = new InlineKeyboardMarkup();
+        //button "Registration"
+        InlineKeyboardButton button = new InlineKeyboardButton("Registration");
+        button.setCallbackData("/registration");
+        //button "About registration"
+        InlineKeyboardButton button1 = new InlineKeyboardButton("About registration");
+        button1.setCallbackData("/aboutRegistration");
+        //positions
+        List<InlineKeyboardButton> row = new ArrayList<>();
+        List<InlineKeyboardButton> row1 = new ArrayList<>();
+        //
+        row.add(button);
+        row1.add(button1);
+
+        List<List<InlineKeyboardButton>> rows = new ArrayList<>();
+        rows.add(row);
+        rows.add(row1);
+
+        keyboardMarkup.setKeyboard(rows);
+
+        SendMessage toSend = new SendMessage();
+        toSend.setChatId(chatId);
+        toSend.setText(msg);
+        toSend.setReplyMarkup(keyboardMarkup);
+        return toSend;
+
     }
 
     private void sendMessage(long chatId, String toSend) {
         SendMessage message = new SendMessage();
         message.setChatId(String.valueOf(chatId));
         message.setText(toSend);
-        try {
-            execute(message);
-        } catch (TelegramApiException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private void sendRegButton(long chatId, String msg, ReplyKeyboardMarkup markup) {
-        SendMessage message = new SendMessage();
-        message.setChatId(String.valueOf(chatId));
-        message.setText(msg);
-        message.setReplyMarkup(markup);
         try {
             execute(message);
         } catch (TelegramApiException e) {
