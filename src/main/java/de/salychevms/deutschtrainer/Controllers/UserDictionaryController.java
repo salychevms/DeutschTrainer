26ff3f67518d6f4ab38aade01a8353df8d7bd0b6
Mikhail Salychev <salychevms@gmail.com>
2023-10-07 19:53:35 +0200

    4 controllers

---
+++ src/main/java/de/salychevms/deutschtrainer/Controllers/UserDictionaryController.java
@@ -0,0 +1,63 @@
+package de.salychevms.deutschtrainer.Controllers;
+
+import de.salychevms.deutschtrainer.Models.MainDictionary;
+import de.salychevms.deutschtrainer.Models.UserDictionary;
+import de.salychevms.deutschtrainer.Models.UserLanguage;
+import de.salychevms.deutschtrainer.Repo.MainDictionaryRepository;
+import de.salychevms.deutschtrainer.Repo.UserDictionaryRepository;
+import de.salychevms.deutschtrainer.Repo.UserLanguageRepository;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class UserDictionaryController {
+    final UserDictionaryRepository userDictionaryRepository;
+    final UserLanguageRepository userLanguageRepository;
+    final MainDictionaryRepository mainDictionaryRepository;
+
+    public UserDictionaryController(UserDictionaryRepository userDictionaryRepository, UserLanguageRepository userLanguageRepository, MainDictionaryRepository mainDictionaryRepository) {
+        this.userDictionaryRepository = userDictionaryRepository;
+        this.userLanguageRepository = userLanguageRepository;
+        this.mainDictionaryRepository = mainDictionaryRepository;
+    }
+
+    public boolean addNewWordFromVMainDictionary(Long wordId, Long userLanguageId) {
+        Optional<UserLanguage> userLanguage = userLanguageRepository.findById(userLanguageId);
+        Optional<MainDictionary> word = mainDictionaryRepository.findById(wordId);
+        if (userLanguage.isPresent() && word.isPresent()) {
+            if (userDictionaryRepository.findWordByUserLanguageIdAndWordId(userLanguageId, wordId).isEmpty()) {
+                UserDictionary userDictionary = new UserDictionary();
+                userDictionary.setWord(word.get());
+                userDictionary.setUserLanguage(userLanguage.get());
+                userDictionary.setAdditionDate(new Date());
+                userDictionaryRepository.save(userDictionary);
+                return true;
+            }else return false;
+        }else return false;
+    }
+
+    public List<UserDictionary> getAllUsersWords(){
+        List<UserDictionary> list=userDictionaryRepository.findAll();
+        if(!list.isEmpty()){
+            return list;
+        }else return null;
+    }
+
+    public List<UserDictionary> getAllWordsByUserLanguageId(Long userId){
+        List<UserDictionary> list=userDictionaryRepository.findAllWordsByUserLanguageId(userId);
+        if(!list.isEmpty()){
+            return list;
+        }else return null;
+    }
+
+    public String getWordByUserDictionaryId(Long userDictionaryId){
+        Optional<UserDictionary> UserDictionaryWordId=userDictionaryRepository.findById(userDictionaryId);
+        if(UserDictionaryWordId.isPresent()){
+            Optional<MainDictionary> MainDictionaryWord=mainDictionaryRepository.findById(UserDictionaryWordId.get().getId());
+            return MainDictionaryWord.map(MainDictionary::getWord).orElse(null);
+        }return null;
+    }
+}
--- src/main/java/de/salychevms/deutschtrainer/Repo/UserDictionaryRepository.java
+++ src/main/java/de/salychevms/deutschtrainer/Repo/UserDictionaryRepository.java
@@ -1,9 +1,18 @@
 package de.salychevms.deutschtrainer.Repo;
 
 import de.salychevms.deutschtrainer.Models.UserDictionary;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface UserDictionaryRepository extends JpaRepository<UserDictionary, Long> {
+    @Transactional
+    Optional<UserDictionary> findWordByUserLanguageIdAndWordId(Long userLanguageId, Long MainDictionaryId);
+
+    @Transactional
+    List<UserDictionary> findAllWordsByUserLanguageId(Long userLanguageId);
 }
