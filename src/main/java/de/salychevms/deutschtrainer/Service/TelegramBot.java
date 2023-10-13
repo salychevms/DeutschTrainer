f7fc8d2d014d0d86c7e20ce95ebec2b8cca39cf9
Mikhail Salychev <salychevms@gmail.com>
2023-10-13 15:16:59 +0200

    13.10.23-1

--- src/main/java/de/salychevms/deutschtrainer/Service/TelegramBot.java
+++ src/main/java/de/salychevms/deutschtrainer/Service/TelegramBot.java
@@ -16,6 +16,7 @@ import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKe
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
@@ -43,53 +44,10 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
+        Message message = update.getMessage();
         //check message
-        if (update.hasCallbackQuery()) {
-            CallbackQuery callbackQuery = update.getCallbackQuery();
-            String callBackData = callbackQuery.getData();
-            //if user exists
-            if (usersController.registeredOr(userName)) {
-                //reg keyboard
-                if (callBackData.equals("/registration")) {
-                    editKeyboard(update.getCallbackQuery(), globalMenu(), "You're already registered!");
-                } else if (callBackData.equals("/aboutRegistration")) {
-                    editKeyboard(update.getCallbackQuery(), globalMenu(), "You're already registered!");
-                } else if (callBackData.equals("/training")) {
-
-                } else if (callBackData.equals("/statistic")) {
-
-                } else if (callBackData.equals("/settings")) {
-                    editKeyboard(update.getCallbackQuery(), settingsMenu(), "Settings");
-                } else if (callBackData.equals("/info")) {
-                    editKeyboard(update.getCallbackQuery(), infoMenu(), "Info");
-                } else if (callBackData.equals("/userInfo")) {
-                    editKeyboard(update.getCallbackQuery(), infoMenu(), sendUserInfo(userName) + "\n\nInfo");
-                } else if (callBackData.equals("/aboutThisBot")) {
-                    editKeyboard(update.getCallbackQuery(), infoMenu(), "I hope this bot will help me and people to study german words very fast!"
-                            + "\nThis bot is free and also it's my opportunity to get new experience as a program developer."
-                            + "\nAll questions you can send me on my email: salychevms@gmail.com."
-                            + "\nGood luck and have fun! =)" + "\n\nInfo");
-                } else if (callBackData.equals("/mainMenu")) {
-                    editKeyboard(update.getCallbackQuery(), globalMenu(), "I'm here!!! Did someone call me???\n\nMain menu");
-                }
-                //if user doesn't exist
-            } else if (!usersController.registeredOr(userName)) {
-                if (callBackData.equals("/registration")) {
-                    usersController.createNewUser(userName);
-                    if (usersController.registeredOr(userName)) {
-                        sendMessage(chatId, "Successfully! You're registered!");
-                        editKeyboard(update.getCallbackQuery(), globalMenu(), "Main menu");
-                    } else sendMessage(chatId, "Oooopsie! Sorry, something wrong happened..." +
-                            "\nWrite \"/start\" and try again!!");
-                } else if (callBackData.equals("/aboutRegistration")) {
-                    editKeyboard(update.getCallbackQuery(), registration(), "You have to registered!\n Please, push *Registration* in this menu.");
-                }
-            } else sendMessage(chatId, "Oooopsie! Sorry, something wrong happened..." +
-                    "\nWrite \"/start\" and try again!!");
-
-            //check global commands
-        } else if (update.hasMessage()) {
-            Message message = update.getMessage();
+        //check global commands
+        if (update.hasMessage()) {
             if (message.hasText()) {
                 String messageText = message.getText();
                 chatId = message.getChatId();
@@ -98,9 +56,8 @@ public class TelegramBot extends TelegramLongPollingBot {
                 if (!usersController.registeredOr(userName)) {
                     //if user sent /start
                     if (messageText.equals("/start")) {
-                        sendMessage(chatId, "Hi, " + userName + "! You activated this bot.");
                         //give start menu
-                        sendKeyboard(registration(), chatId, "You have to register.");
+                        sendKeyboard(registration(), chatId, "You activated this bot." + "\nYou have to register.");
                         //if something unusual happened
                     } else {
                         sendMessage(chatId, "Sorry! It does not works.\nSend command: \"/start\" again, please!");
@@ -115,7 +72,91 @@ public class TelegramBot extends TelegramLongPollingBot {
                     }
                 }
             }
+        } else if (update.hasCallbackQuery()) {
+            CallbackQuery callbackQuery = update.getCallbackQuery();
+            String callBackData = callbackQuery.getData();
+            //if user exists
+            if (usersController.registeredOr(userName)) {
+                //reg keyboard
+                if (callBackData.equals("/training")) {
+                    editKeyboard(update.getCallbackQuery(), trainingMenu(), "Training");
+                } else if (callBackData.equals("/statistic")) {
+                    editKeyboard(update.getCallbackQuery(), statisticMenu(), "Statistic");
+                } else if (callBackData.equals("/settings")) {
+                    editKeyboard(update.getCallbackQuery(), settingsMenu(), "Settings");
+                } else if (callBackData.equals("/info")) {
+                    editKeyboard(update.getCallbackQuery(), infoMenu(), "Info");
+                } else if (callBackData.equals("/userInfo")) {
+                    editKeyboard(update.getCallbackQuery(), infoMenu(), sendUserInfo(userName) + "\n\nInfo");
+                } else if (callBackData.equals("/aboutThisBot")) {
+                    editKeyboard(update.getCallbackQuery(), infoMenu(), "I hope this bot will help me and people to study german words very fast!"
+                            + "\nThis bot is free and also it's my opportunity to get new experience as a program developer."
+                            + "\nAll questions you can send me on my email:\nsalychevms@gmail.com."
+                            + "\nGood luck and have fun! =)" + "\n\nInfo");
+                } else if (callBackData.equals("/changeName")) {
+                    editMessage(callbackQuery, "Change Name");
+                    sendMessage(chatId,"enter new name");
+                } else if (callBackData.equals("/changeSurname")) {
+
+                } else if (callBackData.equals("/setNewLanguage")) {
+
+                } else if (callBackData.equals("/mainMenu")) {
+                    editKeyboard(update.getCallbackQuery(), globalMenu(), "I'm here!!! Did someone call me???\n\nMain menu");
+                }
+                //if user doesn't exist
+            } else if (!usersController.registeredOr(userName)) {
+                if (callBackData.equals("/registration")) {
+                    usersController.createNewUser(userName);
+                    if (usersController.registeredOr(userName)) {
+                        editKeyboard(update.getCallbackQuery(), globalMenu(), "Successfully! You're registered!" + "\n\nMain menu");
+                    } else sendMessage(chatId, "Oooopsie! Sorry, something wrong happened..." +
+                            "\nWrite \"/start\" and try again!!");
+                } else if (callBackData.equals("/aboutRegistration")) {
+                    editKeyboard(update.getCallbackQuery(), registration(), "You have to register!\n\nPlease, push *Registration* in this menu.");
+                }
+            } else sendMessage(chatId, "Oooopsie! Sorry, something wrong happened..." +
+                    "\nWrite \"/start\" and try again!!");
         }
+    }
+
+    private InlineKeyboardMarkup statisticMenu() {
+        InlineKeyboardMarkup keyboardMarkup = new InlineKeyboardMarkup();
+        //button "main menu
+        InlineKeyboardButton goToMainMenuButton = new InlineKeyboardButton("<< main menu");
+        goToMainMenuButton.setCallbackData("/mainMenu");
+        ////position from left to right
+        List<InlineKeyboardButton> row1 = new ArrayList<>();
+        //
+        row1.add(goToMainMenuButton);
+        //position from up to down
+        List<List<InlineKeyboardButton>> rows = new ArrayList<>();
+        rows.add(row1);
+        //save buttons in the markup variable
+        keyboardMarkup.setKeyboard(rows);
+        return keyboardMarkup;
+    }
+
+    private InlineKeyboardMarkup trainingMenu() {
+        InlineKeyboardMarkup keyboardMarkup = new InlineKeyboardMarkup();
+        //button "Daily"
+        InlineKeyboardButton dailyTrainingButton = new InlineKeyboardButton("Daily Training");
+        dailyTrainingButton.setCallbackData("/dailyTraining");
+        //button "main menu
+        InlineKeyboardButton goToMainMenuButton = new InlineKeyboardButton("<< main menu");
+        goToMainMenuButton.setCallbackData("/mainMenu");
+        ////position from left to right
+        List<InlineKeyboardButton> row1 = new ArrayList<>();
+        List<InlineKeyboardButton> row2 = new ArrayList<>();
+        //
+        row1.add(dailyTrainingButton);
+        row2.add(goToMainMenuButton);
+        //position from up to down
+        List<List<InlineKeyboardButton>> rows = new ArrayList<>();
+        rows.add(row1);
+        rows.add(row2);
+        //save buttons in the markup variable
+        keyboardMarkup.setKeyboard(rows);
+        return keyboardMarkup;
     }
 
     private InlineKeyboardMarkup globalMenu() {
@@ -165,8 +206,8 @@ public class TelegramBot extends TelegramLongPollingBot {
         InlineKeyboardButton setNewLanguageButton = new InlineKeyboardButton("Set New Language");
         setNewLanguageButton.setCallbackData("/SetNewLanguage");
         //button "settings menu go back"
-        InlineKeyboardButton goBackButton = new InlineKeyboardButton("<< main menu");
-        goBackButton.setCallbackData("/mainMenu");
+        InlineKeyboardButton goToMainMenuButton = new InlineKeyboardButton("<< main menu");
+        goToMainMenuButton.setCallbackData("/mainMenu");
         //position from left to right
         List<InlineKeyboardButton> row1 = new ArrayList<>();
         List<InlineKeyboardButton> row2 = new ArrayList<>();
@@ -176,7 +217,7 @@ public class TelegramBot extends TelegramLongPollingBot {
         row1.add(changeNameButton);
         row2.add(changeSurnameButton);
         row3.add(setNewLanguageButton);
-        row4.add(goBackButton);
+        row4.add(goToMainMenuButton);
         //position from up to down
         List<List<InlineKeyboardButton>> rows = new ArrayList<>();
         rows.add(row1);
@@ -197,8 +238,8 @@ public class TelegramBot extends TelegramLongPollingBot {
         InlineKeyboardButton aboutBotButton = new InlineKeyboardButton("About This Bot");
         aboutBotButton.setCallbackData("/aboutThisBot");
         //button "About This Bot"
-        InlineKeyboardButton goBackButton = new InlineKeyboardButton("<< main menu");
-        goBackButton.setCallbackData("/mainMenu");
+        InlineKeyboardButton goToMainMenuButton = new InlineKeyboardButton("<< main menu");
+        goToMainMenuButton.setCallbackData("/mainMenu");
         //position from left to right
         List<InlineKeyboardButton> row1 = new ArrayList<>();
         List<InlineKeyboardButton> row2 = new ArrayList<>();
@@ -207,7 +248,7 @@ public class TelegramBot extends TelegramLongPollingBot {
         //
         row1.add(userInfoButton);
         row2.add(aboutBotButton);
-        row3.add(goBackButton);
+        row3.add(goToMainMenuButton);
         //position from up to down
         List<List<InlineKeyboardButton>> rows = new ArrayList<>();
         rows.add(row1);
@@ -264,6 +305,22 @@ public class TelegramBot extends TelegramLongPollingBot {
         message.setMessageId((int) messageId);
         message.setText(msg);
         message.setReplyMarkup(keyboardMarkup);
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void editMessage(CallbackQuery callBackQuery, String msg) {
+        long messageId = callBackQuery.getMessage().getMessageId();
+        long chatId = callBackQuery.getMessage().getChatId();
+
+        EditMessageText message = new EditMessageText();
+
+        message.setText(msg);
+        message.setChatId(chatId);
+        message.setMessageId((int) messageId);
         try {
             execute(message);
         } catch (TelegramApiException e) {
